96fabf6ac3ff0451bf3c121cc9133401e3d227b8
Brennen Lowe <lowebrennen@gmail.com>
2017-12-08 09:52:37 -0800

    turning in hw

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ultimate Cheesburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("In-N-Out 2x2", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Habit Hamburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", 1);
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+var port = 3000;
+var app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require ("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+app.listen(port);
